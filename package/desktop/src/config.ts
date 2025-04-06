79933e1395533148c368ac9ade9fcba6be17bf81
Martin Petr <martinpetrnp@gmail.com>
2025-04-06 21:33:36 +0200

    👷 (desktop): Add deb build target

--- package/desktop/src/config.ts
+++ package/desktop/src/config.ts
@@ -1,2 +1,5 @@
-export const APP_URL = "https://app.birdiemail.social";
-//export const APP_URL = "http://localhost:3001";
+//export const APP_URL = "https://app.birdiemail.social";
+export const APP_URL =
+  process.env.NODE_ENV == "development"
+    ? "http://localhost:3001"
+    : "https://app.birdiemail.social";
